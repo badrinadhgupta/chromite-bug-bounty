5b62c73b39787d49ba7fecf7a1042bbe188ef13e
Badri Nerella <49241415+badrinadhgupta@users.noreply.github.com>
2021-12-26 16:41:58 +0530

    Update uatg_csrbox_misam.py

--- uatg_csrbox_misam.py
+++ uatg_csrbox_misam.py
@@ -21,7 +21,7 @@ class uatg_csrbox_misam(IPlugin):
           x1 = random.randrange(0,2**16+1)
           x2 = random.randrange(0,2**16+1)
           asm += f'\tli x1, {hex(x1)}\n\tli x2, {hex(x2)}\n\tmul x3, x1, x2\n\tmulh x4, x1, x2\n\tmulhu x5, x1, x2\n\tmulhsu x6, x1, x2\n\tdiv x7, x1, x2\n\tdivu x8, x1, x2\n\tcsrrc x30, misa, 0x1000\n\tmul x9, x1, x2\n\tmulh x10, x1, x2\n\tmulhu x11, x1, x2\n\tmulhsu x12, x1, x2\n\tdiv x13, x1, x2\n\tdivu x14, x1, x2\n\tbeq x9, x3, trap\n\tbeq x10, x4, trap\n\tbeq x11, x5, trap\n\tbeq x12, x6, trap\n\tbeq x13, x7, trap\n\tbeq x14, x8, trap\n\tcsrrs x30, misa, 0x1000\n\tmul x15, x1, x2\n\tmulh x16, x1, x2\n\tmulhu x17, x1, x2\n\tmulhsu x18, x1, x2\n\tdiv x19, x1, x2\n\tdivu x20, x1, x2\n\tbne x15, x3, trap\n\tbne x16, x4, trap\n\tbne x17, x5, trap\n\tbne x18, x6, trap\n\tbne x19, x7, trap\n\tbne x20, x8, trap\n'
-        asm += 'trap:\n\taddi x31, x0, 1\n'
+        asm += 'trap:\n\taddi x30, x1, 0\n\taddi x31, x2, 0\n'
 
         compile_macros = []
 
