c20788993c288935dafd36cc0344350d111a0aa8
Badri Nerella <49241415+badrinadhgupta@users.noreply.github.com>
2021-12-26 13:25:51 +0530

    Update uatg_csrbox_infocsr2.py

--- uatg_csrbox_infocsr2.py
+++ uatg_csrbox_infocsr2.py
@@ -22,7 +22,8 @@ class uatg_csrbox_test1(IPlugin):
         for j in range(0, len(csr)):
           for i in range(0,200):
             x = random.randrange(0,2**32)
-            asm += f'\tli x3, {hex(x)}\n\tcsrrs x4, {csr[j]}, x3\n\tbeq x3, {csr[j]}, trap\n'
+            asm += f'\tli x3, {hex(x)}\n\tcsrr x2, {csr[j]}\n\tcsrrs x4, {csr[j]}, x3\n\tbnq x2, {csr[j]}, trap\n'
+	    asm += f'\tcsrrsi x4, {csr[j]}, {hex(x)}\n\tbnq x2, {csr[j]}, trap\n'
         asm += 'trap:\n\taddi x31, x31, 1\n'
 
         compile_macros = []
